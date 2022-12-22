eca9b6b89d45cbd926f19b85e24bb094d2a471a7
nichol20 <nick.nogue28@outlook.com>
2022-12-22 16:50:05 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import express, { NextFunction, Request, Response } from 'express'
+import nodemailer from 'nodemailer'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cors({
+  origin: process.env.FRONTEND_URL
+}))
+
+// Global error handling
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.log(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+app.post('/email', async (req, res) => {
+  const { email, subject, message, name } = req.body
+
+  if(!email || !subject || !message || !name) return res.status(400).json({ message: 'Missing information'})
+
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.SENDER_EMAIL, 
+        pass: process.env.SENDER_PASSWORD, 
+      },
+    });
+    let info = await transporter.sendMail({
+      from: `${name} <${email}>`, 
+      to: process.env.RECIPIENT_EMAIL, 
+      subject: String(subject), 
+      text: String(message)
+    });
+    console.log("Message sent: %s", info.messageId);
+    return res.status(200).json({ message: 'Send successfully'})
+  } catch (error) {
+    console.log(error)
+    return res.status(400)
+  }
+
+})
+
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}🥵🥶🤡`))
