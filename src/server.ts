e429f1fc90b294a53ce71e0805621860ba6e07a6
Nicholas <nick.nogue28@outlook.com>
2025-01-16 12:27:06 -0300

    change

--- src/server.ts
+++ src/server.ts
@@ -9,11 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-  })
-);
 
 // Global error handling
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
@@ -21,38 +16,46 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).send("Something broke!");
 });
 
-app.post("/email", async (req, res) => {
-  const { email, subject, message } = req.body;
+app.get("/", (req, res) => {
+  return res.send("My portfolio server🥵🥶🤡");
+});
 
-  if (!email || !subject || !message)
-    return res.status(400).json({ message: "Missing information" });
+app.post(
+  "/email",
+  cors({ origin: process.env.FRONTEND_URL }),
+  async (req, res) => {
+    const { email, subject, message } = req.body;
 
-  try {
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.SENDER_EMAIL,
-        pass: process.env.SENDER_PASSWORD,
-      },
-    });
-    let info = await transporter.sendMail({
-      from: `<${email}>`,
-      to: process.env.RECIPIENT_EMAIL,
-      subject: String(subject),
-      text: `
+    if (!email || !subject || !message)
+      return res.status(400).json({ message: "Missing information" });
+
+    try {
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.SENDER_EMAIL,
+          pass: process.env.SENDER_PASSWORD,
+        },
+      });
+      let info = await transporter.sendMail({
+        from: `<${email}>`,
+        to: process.env.RECIPIENT_EMAIL,
+        subject: String(subject),
+        text: `
         from: <${email}> 
         message:
         ${message}
       `,
-    });
-    console.log("Message sent: %s", info.messageId);
-    return res.status(200).json({ message: "Send successfully" });
-  } catch (error) {
-    console.log(error);
-    return res.status(400);
+      });
+      console.log("Message sent: %s", info.messageId);
+      return res.status(200).json({ message: "Send successfully" });
+    } catch (error) {
+      console.log(error);
+      return res.status(400);
+    }
   }
-});
+);
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}🥵🥶🤡`));
