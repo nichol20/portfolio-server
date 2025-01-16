5a9145023019b447c5437240368357a69adb3707
Nicholas <nick.nogue28@outlook.com>
2025-01-16 12:06:19 -0300

    add vercel and add env.example

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,31 @@
-import express, { NextFunction, Request, Response } from 'express'
-import nodemailer from 'nodemailer'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express, { NextFunction, Request, Response } from "express";
+import nodemailer from "nodemailer";
+import cors from "cors";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
-app.use(cors({
-  origin: process.env.FRONTEND_URL
-}))
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
 // Global error handling
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  console.log(err.stack)
-  res.status(500).send('Something broke!')
-})
+  console.log(err.stack);
+  res.status(500).send("Something broke!");
+});
 
-app.post('/email', async (req, res) => {
-  const { email, subject, message, name } = req.body
+app.post("/email", async (req, res) => {
+  const { email, subject, message } = req.body;
 
-  if(!email || !subject || !message || !name) return res.status(400).json({ message: 'Missing information'})
+  if (!email || !subject || !message)
+    return res.status(400).json({ message: "Missing information" });
 
   try {
     let transporter = nodemailer.createTransport({
@@ -30,27 +33,26 @@ app.post('/email', async (req, res) => {
       port: 587,
       secure: false,
       auth: {
-        user: process.env.SENDER_EMAIL, 
-        pass: process.env.SENDER_PASSWORD, 
+        user: process.env.SENDER_EMAIL,
+        pass: process.env.SENDER_PASSWORD,
       },
     });
     let info = await transporter.sendMail({
-      from: `${name} <${email}>`, 
-      to: process.env.RECIPIENT_EMAIL, 
-      subject: String(subject), 
+      from: `<${email}>`,
+      to: process.env.RECIPIENT_EMAIL,
+      subject: String(subject),
       text: `
-        from: ${name} <${email}> 
+        from: <${email}> 
         message:
         ${message}
-      `
+      `,
     });
     console.log("Message sent: %s", info.messageId);
-    return res.status(200).json({ message: 'Send successfully'})
+    return res.status(200).json({ message: "Send successfully" });
   } catch (error) {
-    console.log(error)
-    return res.status(400)
+    console.log(error);
+    return res.status(400);
   }
+});
 
-})
-
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}🥵🥶🤡`))
+app.listen(PORT, () => console.log(`Server is running on port: ${PORT}🥵🥶🤡`));
